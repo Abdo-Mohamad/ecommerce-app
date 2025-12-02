1068866bcb03d5f43d06b82f67df790910c3782b
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2025-12-02 18:24:29 +0100

    Define and configure all application routes in App.jsx using React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/HOME.jsx'
+import About from './pages/ABOUT.jsx'
+import Contact from './pages/Contact.jsx'
+import Collection from './pages/collection.jsx'
+import Product from './pages/Product.jsx'
+import PlaceOrder from './pages/PlaceOrder.jsx'
+import Cart from './pages/Cart.jsx'
+import Login from './pages/Login.jsx'
+
 
 const App = () => {
   return (
-    <div>
-
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/placeorder' element={<PlaceOrder />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
     </div>
   )
 }
